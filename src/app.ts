d7c0c48bb8c427df45f2d98eef322b9f22304a4c
VASHvic <vforvanish@gmail.com>
2022-05-08 13:23:12 +0200

    Squashed commit of the following:
    commit d734820b83295fa0c52ba05c4c1673c06e743d42
    Author: VASHvic <vforvanish@gmail.com>
    Date:   Sun May 8 13:17:14 2022 +0200
    
        primera versió api acabada - TODO: millorar la clase de conexió
    
    commit b3606f7980ac93f22e3214ada14326cf5fb86224
    Author: VASHvic <vforvanish@gmail.com>
    Date:   Sat May 7 20:10:43 2022 +0200
    
        added types + try catch
    
    commit e1b5838fb3864288199d3c3828b6097ce63c6372
    Author: VASHvic <vforvanish@gmail.com>
    Date:   Sat May 7 19:18:43 2022 +0200
    
        encapsulat funciona

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,70 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
-import {
-  getAll,
-  insertFullData,
-  getLastInfo,
-  getAllInfoFromId,
-  getAllLast,
-} from './database/database';
+import {MongoService} from './database/database';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (_, res) => {
-  res.json({routes: {get: ['/', '/getall', 'get/:id'], post: ['/']}});
+const mongo = new MongoService();
+
+app.get('/', (_, res: Response): Object => {
+  return res.json({
+    routes: {
+      get: ['/', '/getall', '/getallids', '/getall/last', 'getall/:id'],
+      post: ['/'],
+    },
+  });
 });
 
-app.get('/getall', async (_, res) => {
-  const documents = await getAll();
-  res.send(documents);
-});
-app.get('/getall/last', async (_, res) => {
-  const documents = await getAllLast();
-  res.send(documents);
-});
-
-app.get('/getall/:id', async (req, res) => {
-  const sensorId = req.params.id;
-  const last = await getAllInfoFromId(sensorId);
-  res.send(last);
-});
-
-app.get('/get/:id', async (req, res) => {
-  const sensorId = req.params.id;
-  const info = await getLastInfo(sensorId);
-  res.send(info);
-});
-
-app.post('/', async (req, res) => {
+app.get('/getall', async (_, res: Response): Promise<Response<JSON[] | Error>> => {
   try {
-    const [data] = req.body.data;
-    console.log(data.id);
-
-    if (data.id) {
-      req.body.myId = data.id;
-    }
-    const insert = await insertFullData(req.body);
-    res.send(insert);
+    const collection = await mongo.connect();
+    const documents = await mongo.getAll(collection);
+    return res.send(documents);
   } catch (err) {
-    res.json({
-      error: {
-        message: err.message,
-      },
-    });
+    return res.status(500).json({error: err.message});
   }
+});
+app.get('/getallids', async (_, res: Response) => {
+  try {
+    const collection = await mongo.connect();
+    const ids = await mongo.getAllSensorIds(collection);
+    return res.send(ids);
+  } catch (err) {
+    return res.status(500).json({error: err.message});
+  }
+});
+
+app.get('/getall/last', async (_, res: Response): Promise<Response<JSON[] | Error>> => {
+  try {
+    const collection = await mongo.connect();
+    const documents = await mongo.getAllLast(collection);
+    return res.send(documents);
+  } catch (err) {
+    return res.status(500).json({error: err.message});
+  }
+});
+app.get('/getall/:id', async (req: Request, res: Response) => {
+  const collection = await mongo.connect();
+  const sensorId = req.params?.id;
+  const last = await mongo.getAllInfoFromId(collection, sensorId);
+  return res.send(last);
+});
+
+app.post('/', async (req: Request, res: Response): Promise<Response<boolean | Error>> => {
+  try {
+    const collection = await mongo.connect();
+    const insert = await mongo.insertFullData(collection, req.body);
+    return res.send(insert);
+  } catch (err) {
+    return res.status(500).json({error: err.message});
+  }
+});
+
+app.get('*', (_, res: Response): Response<String> => {
+  return res.status(301).send('Route not Found');
 });
 
 export {app};
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,87 +1,78 @@
-import {MongoClient} from 'mongodb';
+import {Collection, MongoClient} from 'mongodb';
 import {MONGODB_URI} from '../config';
 
-const client = new MongoClient(MONGODB_URI);
-//TODO: Convertir en clase?
-export async function getAll(): Promise<any[] | Error> {
-  try {
-    await client.connect();
-    const database = client.db('sensors');
-    const collection = database.collection('brokerData');
-    return await collection.find({}).sort({$natural: -1}).toArray();
-  } catch (err) {
-    return err;
-  }
-}
-export async function getAllLast(): Promise<any[] | Error> {
-  try {
-    await client.connect();
-    const database = client.db('sensors');
-    const collection = database.collection('brokerData');
-    return await collection
-      .aggregate([
-        {
-          $unwind: {
-            path: '$data',
-          },
-        },
-        {
-          $sort: {
-            'data.LAeq.metadata.TimeInstant.value': -1,
-          },
-        },
-        {
-          $group: {
-            _id: '$data.id',
-            doc: {$first: '$$ROOT'},
-          },
-        },
-      ])
-      .toArray();
-  } catch (err) {
-    return err;
-  }
-}
-export async function getLastInfo(sensorId: string): Promise<any> {
-  const query = {
-    data: {$elemMatch: {id: `NoiseLevelObserved-HOPVLCi${sensorId}`}},
-  };
-  const options: any = {
-    sort: {$natural: -1},
-  };
+export class MongoService {
+  client = new MongoClient(MONGODB_URI || '');
 
-  try {
-    await client.connect();
-    const database = client.db('sensors');
-    const collection = database.collection('brokerData');
-    return await collection.findOne(query, options);
-  } catch (err) {
-    return err;
+  async connect() {
+    try {
+      await this.client.connect();
+      return this.client.db('sensors').collection('brokerData');
+    } catch (err) {
+      return err;
+    }
   }
-}
-//TODO: afegir projecció als datos que fan falta per al array de la gráfica
-export async function getAllInfoFromId(sensorId: string): Promise<any> {
-  const query = {
-    data: {$elemMatch: {id: `NoiseLevelObserved-HOPVLCi${sensorId}`}},
-  };
 
-  try {
-    await client.connect();
-    const database = client.db('sensors');
-    const collection = database.collection('brokerData');
-    return await collection.find(query).toArray();
-  } catch (err) {
-    return err;
+  async getAll(collection: Collection) {
+    try {
+      return await collection.find({}).sort({$natural: -1}).toArray();
+    } catch (err) {
+      return err;
+    }
   }
-}
+  async getAllLast(collection: Collection) {
+    try {
+      return await collection
+        .aggregate([
+          {
+            $unwind: {
+              path: '$data',
+            },
+          },
+          {
+            $sort: {
+              'data.LAeq.metadata.TimeInstant.value': -1,
+            },
+          },
+          {
+            $group: {
+              _id: '$data.id',
+              doc: {$first: '$$ROOT'},
+            },
+          },
+        ])
+        .toArray();
+    } catch (err) {
+      return err;
+    }
+  }
+  async insertFullData(collection: Collection, json: any): Promise<boolean | unknown> {
+    try {
+      const {acknowledged} = await collection.insertOne(json);
+      return acknowledged;
+    } catch (err: unknown) {
+      return err;
+    }
+  }
+  async getAllInfoFromId(
+    collection: Collection,
+    sensorId: string
+  ): Promise<any[] | unknown> {
+    const query = {
+      data: {$elemMatch: {id: `NoiseLevelObserved-HOPVLCi${sensorId}`}},
+    };
+    try {
+      return await collection.find(query).toArray();
+    } catch (err: unknown) {
+      return err;
+    }
+  }
 
-export async function insertFullData(json: any): Promise<any> {
-  try {
-    await client.connect();
-    const database = client.db('sensors');
-    const collection = database.collection('brokerData');
-    return await collection.insertOne(json);
-  } catch (err) {
-    return err;
+  async getAllSensorIds(collection: Collection): Promise<String[] | unknown> {
+    try {
+      return await collection.distinct('data.id');
+    } catch (err) {
+      return err;
+    }
   }
 }
