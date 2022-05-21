e6aa684382f96aad68ee3de1e340337551240ad1
VASHvic <vforvanish@gmail.com>
2022-05-21 16:45:02 +0200

    ADDED getone/:id route and minor fixes

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,10 @@
-import {Collection, MongoClient} from 'mongodb';
+import {Collection, Document, MongoClient} from 'mongodb';
 import {MONGODB_URI} from '../config';
 
 export class MongoService {
   client = new MongoClient(MONGODB_URI);
 
-  async connect() {
+  async connect(): Promise<Collection> {
     try {
       await this.client.connect();
       return this.client.db('sensors').collection('brokerData');
@@ -13,14 +13,14 @@ export class MongoService {
     }
   }
 
-  async getAll(collection: Collection) {
+  async getAll(collection: Collection): Promise<Document[] | Error> {
     try {
       return await collection.find({}).sort({$natural: -1}).toArray();
     } catch (err) {
       return err;
     }
   }
-  async getAllLast(collection: Collection) {
+  async getAllLast(collection: Collection): Promise<Document[] | Error> {
     try {
       return await collection
         .aggregate([
@@ -48,18 +48,51 @@ export class MongoService {
       return err;
     }
   }
-  async insertFullData(collection: Collection, json: any): Promise<boolean | unknown> {
+  async getLastOne(
+    collection: Collection,
+    sensorId: string
+  ): Promise<Document[] | Error> {
+    console.log(sensorId);
+    try {
+      return await collection
+        .find({data: {$elemMatch: {id: `NoiseLevelObserved-HOPVLCi${sensorId}`}}})
+        .project({
+          _id: 0,
+          subscriptionId: 0,
+          data: {
+            type: 0,
+            LA99: 0,
+            LAMax: 0,
+            LAMin: 0,
+            LA1: 0,
+            LA50: 0,
+            LA10: 0,
+            location: 0,
+            dateObservedFrom: 0,
+            dateObservedTo: 0,
+            operationalStatus: 0,
+            noiseLevelLAEq: 0,
+          },
+        })
+        .sort({$natural: -1})
+        .limit(1)
+        .toArray();
+    } catch (err) {
+      return err;
+    }
+  }
+  async insertFullData(collection: Collection, json: any): Promise<boolean | Error> {
     try {
       const {acknowledged} = await collection.insertOne(json);
       return acknowledged;
-    } catch (err: unknown) {
+    } catch (err) {
       return err;
     }
   }
   async getAllInfoFromId(
     collection: Collection,
     sensorId: string
-  ): Promise<any[] | unknown> {
+  ): Promise<Document[] | Error> {
     const query = {
       data: {$elemMatch: {id: `NoiseLevelObserved-HOPVLCi${sensorId}`}},
     };
@@ -85,12 +118,12 @@ export class MongoService {
           },
         })
         .toArray();
-    } catch (err: unknown) {
+    } catch (err) {
       return err;
     }
   }
 
-  async getAllSensorIds(collection: Collection): Promise<String[] | unknown> {
+  async getAllSensorIds(collection: Collection): Promise<String[] | Error> {
     try {
       return await collection.distinct('data.id');
     } catch (err) {
